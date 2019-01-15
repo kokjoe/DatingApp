ed83452faaabb8c789bf896599ed41146cc0c9de
Ng <kokjoe.ng@cgm.com>
2019-01-15 09:51:15 +0800

    end of section 11 - cloudinary upload photo

--- DatingApp-SPA/src/app/_models/user.ts
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -8,7 +8,7 @@ export interface User {
     gender: string;
     created: Date;
     lastActive: Date;
-    photoUrl: String;
+    photoUrl: string;
     city: string;
     country: string;
     interests?: string;
--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dto;
 using DatingApp.API.Models;
@@ -19,11 +20,13 @@ namespace DatingApp.API.Controllers
     {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
 
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
         {
             _repo = repo;
             _config = config;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -52,38 +55,42 @@ namespace DatingApp.API.Controllers
         {
             // try
             // {
-               // throw new Exception("Computer says no!");
-                
-                var userFromRepo = await _repo.Login(userforLoginDto.Username.ToLower(), userforLoginDto.Password);
+            // throw new Exception("Computer says no!");
 
-                if (userFromRepo == null)
-                    return Unauthorized();
+            var userFromRepo = await _repo.Login(userforLoginDto.Username.ToLower(), userforLoginDto.Password);
 
-                var claims = new[]
-                {
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
                     new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
                     new Claim(ClaimTypes.Name, userFromRepo.Username)
-                }; 
-
-                var key = new SymmetricSecurityKey(Encoding.UTF8
-                    .GetBytes(_config.GetSection("AppSettings:Token").Value));  
-
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = new ClaimsIdentity(claims),
-                    Expires = DateTime.Now.AddDays(1),
-                    SigningCredentials = creds
                 };
 
-                var tokenHandler = new JwtSecurityTokenHandler(); 
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
 
-                var token = tokenHandler.CreateToken(tokenDescriptor);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-                return Ok(new {
-                    token = tokenHandler.WriteToken(token)
-                });
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                user
+            });
 
             // } catch 
             // {
